ab0ea054b03d8c36606f4a6e837df4bf23edf9de
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-12-01 23:56:40 -0800

    Add city database from json file for weather

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "******I'm going to start save date from city_list to database ********"
+
+city_list = JSON.parse(File.read('city_list.json'))
+city_list.each do |city| 
+    City.create(city_id: city['id'], name: city['name'], country: city['country'])
+end
+
